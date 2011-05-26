39a9f455bb737e3a08921d61d15714febf2e0617
Eder Machado <eder@infoclad.com.br>
2011-05-26 11:31:51 -0300

    index

--- Infoclad-panel/index.php
+++ Infoclad-panel/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-	<head>
-		<?php include("head.php"); ?>
-	</head>
-	<body onload="letra();">
-		<?php include("header.php"); ?>
-		<?php include("home.php"); ?>
-		<?php include("footer.php");?>
-	</body>
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+    <body onload="letra();">
+        <?php include("header.php"); ?>
+        <?php include("home.php"); ?>
+        <?php include("footer.php"); ?>
+    </body>
 </html>
