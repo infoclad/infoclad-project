c3b77d7a8bb23caf8de497498c44e2034494e93c
Eder <eder@infoclad.com.br>
2011-05-31 15:41:02 -0300

    raul e duchao banners

---
+++ Raul/head.php
@@ -0,0 +1,13 @@
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<title></title>
+		<link rel="stylesheet" href="css/default.css" type="text/css" media="screen" />
+		<!--[if lte IE 7]>
+			<link rel="stylesheet" href="css/ie.css" type=text/css media="screen" />
+		<![endif]-->
+		<script type="text/javascript" src="js/functions.js"></script>
+		<script type="text/javascript" src="js/jquery-1.4.3.min.js"></script>
+
+
+
+		
+		
---
+++ Raul/includes/home.php
@@ -0,0 +1,45 @@
+<div id="relatorio" class="wrapper-holder">
+    <div class="wrapper">
+		<div id="banner-holder" class="b wrapper">
+			<div id="banner" class="wrapper">
+				<div id="bannerDynamicContent">
+					<div class="b switchProduct">
+						<div id="bannerProduct" class="b l">
+								<div class="b l modelo"><img src="img/produtos/modelo/modelo00.gif"></div>
+								<div class="b l produto"><img src="img/produtos/produto/produto00.gif"></div>
+								<div class="b r titulo">
+									<h1 class="titulo">Lorem Ipsum</h1>
+									<h2 class="copy">Por: Raul Santana</h2>
+								</div>
+								<div class="b estampa"><img src="img/produtos/estampa/estampa00.gif"></div>
+								<div class="c"><!--  --></div>					
+						</div>
+						<div id="bannerProductInfo" class="b r">
+							<span class="b"><h1 class="oferta">Esta oferta pode ser adiquirida em:</h1></span>
+							<span class="b countdown">teste</span>
+							<span class="b"><h1>Apenas:</h1></span>
+							<span class="b"><h1 class="preco">R$37,90</h1></span>
+							<span class="b p"><!--  --></span>
+							<span class="b"><h1>desconto de 50%</h1></span>
+						</div>
+						<div class="c"><!--  --></div>
+					</div>
+				</div>
+				<div id="bannerInfo" class="b r">
+					<!--  -->
+				</div>
+				<div id="bannerSocial" class=" b r">
+					<a href="#"><span class="b l sprite-y facebook"><!--  --></span></a>
+					<a href="#"><span class="b l sprite-y twitter"><!--  --></span></a>
+					<a href="#"><span class="b l sprite-y wp"><!--  --></span></a>
+					<a href="#"><span class="b l sprite-y vimeo"><!--  --></span></a>
+					<a href="#"><span class="b l sprite-y youtube"><!--  --></span></a>
+					<a href="#"><span class="b l sprite-y rss"><!--  --></span></a>
+					<div class="c"><!--  --></div>
+				</div>
+				<div class="c"><!--  --></div>
+			</div>
+			<div class="c"><!--  --></div>
+		</div>
+    </div>
+</div>
