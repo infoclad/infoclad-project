0ecb07a16ed5ca18c3bdac9e262553fa7022b07c
eder@infoclad.com <Wes@.(none)>
2011-06-01 19:18:39 -0300

    raul

---
+++ academia-infoclad/includes/breadcrumb.php
@@ -0,0 +1,10 @@
+<!-- breadcrumb module -->
+<span id="breadcrumb" class="b wrapper-holder">
+    <div class="wrapper">
+        <ul class="inline">
+            <li><a href="#">Página inicial</a></li>
+            <!-- <li>&#8592;</li>  -->
+        </ul>
+    </div>
+</span>
+<!-- breadcrumb module -->
---
+++ academia-infoclad/includes/mainHorizontalMenu.php
@@ -0,0 +1,60 @@
+<!-- horizontal menu module -->
+<div id="mainMenuHolder">
+    <div id="mainMenu" class="b wrapper-holder sprite-x">
+        <div class="wrapper">
+            <ul class="inline">
+                <li>
+                    <a href="?page=academia"><div class="b l mainLink">A Academia</div></a>
+                </li>
+                <li class="aSub">
+                    <a href="?page=infoclad"><div class="b l mainLink">Conheça a infoclad</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=fornecedores">gerenciar fornecedores</a></li>
+                        <li><a href="?page=cadastroFornecedores">adicionar fornecedores</a></li>
+                        <li><a href="?page=cadastroFornecedores">editar fornecedor</a></li>
+                    </ul>
+                </li>
+                <li class="aSub">
+                    <a href="?page=instalacao"><div class="b l mainLink">Nossas instalações</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=produtos">gerenciar produtos</a></li>
+                        <li><a href="?page=cadastroProdutos">adicionar novo produto</a></li>
+                        <li><a href="?page=cadastroProdutos">editar produto</a></li>
+                    </ul>
+                </li>
+                <li class="aSub">
+                    <a href="?page=cursos"><div class="b l mainLink">Nossos cursos</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=categorias">gerenciar categorias</a></li>
+                        <li><a href="?page=cadastroCategorias">adicionar nova categoria</a></li>
+                        <li><a href="?page=cadastroCategorias">editar categoria</a></li>
+                    </ul>
+                </li>
+                <li class="aSub">
+                    <a href="?page=cadastro"><div class="b l mainLink">Cadastre-se</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=pedidos">gerenciar pedidos</a></li>
+                        <li><a href="?page=pedidosDetalhes">adicionar novo pedido</a></li>
+                        <li><a href="?page=pedidosDetalhes">editar pedido</a></li>
+                    </ul>
+                </li>	
+                <li class="aSub">
+                    <a href="?page=grade"><div class="b l mainLink">Grade de horários</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=pedidos">gerenciar pedidos</a></li>
+                        <li><a href="?page=pedidosDetalhes">adicionar novo pedido</a></li>
+                        <li><a href="?page=pedidosDetalhes">editar pedido</a></li>
+                    </ul>
+                </li>	
+                <li>
+                    <a href="?page=aluno"><div class="b l mainLink">Área do aluno</div></a>
+                </li>
+                <li>
+                    <a href="?page=fale"><div class="b l mainLink">Fale conosco</div></a>
+                </li>
+                <div class="c"><!--  --></div>				
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- horizontal menu module -->
