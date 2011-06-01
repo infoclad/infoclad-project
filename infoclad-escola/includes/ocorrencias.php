6d7f00cd7681ee6e9500f6f87ef73555e4b80692
Eder <eder@infoclad.com.br>
2011-06-01 12:39:25 -0300

    painel

---
+++ infoclad-escola/includes/ocorrencias.php
@@ -0,0 +1,16 @@
+<div id="ocorrencias" class="wrapper-holder">
+    <div class="wrapper">
+        <?php include("includes/organizerModule.php"); ?>
+        <span class="b p5"><!--  --></span>
+        <span class="b">
+            <table class="listagem">
+                <tr class="post"><td><strong>15/10/2000</strong> - Ut dapibus varius augue, sit amet malesuada ligula tincidunt lobortis.</td></tr>
+                <tr class="post"><td><strong>02/10/2000</strong> - Sed fringilla enim sit amet magna rhoncus in tincidunt ligula dignissim.</td></tr>
+            </table>
+            <?php include("includes/paginacaoModule.php"); ?>
+        </span>
+        <span class="b p5"><!--  --></span>
+
+        <div class="c"><!--  --></div>
+    </div>
+</div>
---
+++ infoclad-escola/includes/topBar.php
@@ -0,0 +1,22 @@
+<div id="topBar" class="b wrapper-holder">
+    <div class="wrapper p5">
+        <span class="b l p5">
+            <span id="loggedAvatar" class="b l"><img src="img/loggedUsr.png"/></span>
+            <span class="b l p3"><!--  --></span>
+            <span class="b l">Seja Bem Vindo(a), nameUsr.</span>
+            <span class="b l p3"><!--  --></span>
+            <span class="b l"><a class="whiteText" href="#">(<span class="bolder">Sair</span>)</a></span>
+            <div class="c"><!--  --></div>
+        </span>
+        <span class="b r p5">
+            <?php include("includes/msgModule.php"); ?>
+            <span class="b l p3"><!--  --></span>
+            <span class="b l sprite minha_conta"><!--  --></span>			
+            <span class="b l"><a class="whiteText" href="?page=usrProfile">Acessar Minha Conta</a></span>
+            <span class="b l p3"><!--  --></span>
+            <?php include("navProjects.php"); ?>
+            <div class="c"><!--  --></div>
+        </span>
+        <div class="c"><!--  --></div>
+    </div>
+</div>
