ea44b59d998c9a47e0b7632158e2a0ffbe5f3a8c
Eder <eder@infoclad.com.br>
2011-06-02 17:38:41 -0300

    Erro na nav

--- academia-infoclad/nav.php
+++ academia-infoclad/nav.php
@@ -4,6 +4,6 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'relatorio';
 if (file_exists("includes/{$page}.php") == true) {
     include("includes/{$page}.php");
 } else {
-    include('includes/relatorio.php');
+    include('includes/home.php');
 }
 ?>
