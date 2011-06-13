f4e833a40a8190a21dbc715aae793af08cf0da4b
Eder <eder@infoclad.com.br>
2011-06-13 17:32:03 -0300

    infoclad empresa home

---
+++ infoclad_empresa/includes/footer.php
@@ -0,0 +1,67 @@
+<div class="wrapper">
+<div class="b footer-header">
+	<ul class="b l ">
+		<li><h1>A INFOCLAD</h1></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=index">Home</a></li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li><h1>Academia Infoclad</h1></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+	</ul>
+	<ul class="b l ">
+		<li><h1>Soluções</h1></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+	</ul>
+	<ul class="b l ">
+		<li><h1>Serviços</h1></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+	</ul>
+	<ul class="b l ">
+		<li><h1>Cursos</h1></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li><a href="?page=">Lorem Ipsum</a></li>
+		<li>&nbsp;</li>
+		<li>&nbsp;</li>
+		<li><h1><a href="?page=">Contate-nos</a></h1></li>
+	</ul>
+	<div class="b r p10"><a href="http://www.infoclad.com.br"><img src="img/copyright.gif"/></a></div>
+	<div class="c"><!--  --></div>
+	
+</div>
+<div class="b footer-footer">
+	<span class="b l">
+		<ul class="inline">
+			<li><a href="?page=">Webmail</a></li>
+			<li>|</li>
+			<li><a href="?page=">Registre seu domínio</a></li>
+			<li>|</li>
+			<li><a href="?page=">Política de privacidade</a></li>
+			<li>|</li>
+			<li><a href="?page=">Fale Conosco</a></li>
+		</ul>
+	</span>
+	<span class="b r">INFOCLAD - Integrando Tecnologia ao seu Negócio  © 2011 Todos os direitos reservados.</span>
+	<div class="c p10"><!--  --></div>
+	<span class="b"><p align="justify">Duis adipiscing eleifend gravida. Curabitur mi metus, viverra at placerat vitae, hendrerit ut lorem. Suspendisse semper ante et neque accumsan accumsan. Praesent ullamcorper vehicula mauris. 
+	Etiam et sapien quis sapien elementum adipiscing. Nulla blandit fringilla iaculis. Ut non justo velit. Nullam ac mauris ut elit consectetur mattis et sed diam. Etiam consectetur mauris eu sapien lobortis 
+	sit amet lobortis turpis adipiscing. Nulla facilisi. Ut et dui nulla. Cras tincidunt dapibus risus ac tempus. Suspendisse congue velit vel metus mollis et lacinia nunc varius.
+	Duis adipiscing eleifend gravida. Curabitur mi metus, viverra at placerat vitae, hendrerit ut lorem. Suspendisse semper ante et neque accumsan accumsan. Praesent ullamcorper vehicula mauris. 
+	Etiam et sapien quis sapien elementum adipiscing. Nulla blandit fringilla iaculis. Ut non justo velit. Nullam ac mauris ut elit consectetur mattis et sed diam. Etiam consectetur mauris eu sapien lobortis 
+	sit amet lobortis turpis adipiscing. Nulla facilisi. Ut et dui nulla. Cras tincidunt dapibus risus ac tempus. Suspendisse congue velit vel metus mollis et lacinia nunc varius.
+	<br/><br/>
+	Infoclad Solutions, 2011.
+	</p>
+	</span>
+</div>
+<div class="c"><!--  --></div>
+</div>
--- infoclad_empresa/includes/home.php
+++ infoclad_empresa/includes/home.php
@@ -1,1 +1,59 @@
 <?php include("includes/headlines.php"); ?>
+<div id="barra-busca" class="b">
+	<div class="b barra-busca">
+		<span id="menu"  class="b l ">
+		<a class="b p10" href="#">Menu</a>
+		<div class="menu">
+			<span class="b p10">
+				<ul class="b l">
+					<li><a href="?page=index">home</a></li>
+					<li><a href="?page=infoclad">a INFOCLAD</a></li>
+					<li><a href="?page=academia">academia infoclad</a></li>
+				</ul>
+				<span class="b l p10"><!--  --></span>
+				<ul class="b l">
+					<li><a href="?page=servicos">serviços</a></li>
+					<li><a href="?page=solucoes">soluções</a></li>
+				</ul>
+				<span class="b l p10"><!--  --></span>
+				<ul class="b l">
+					<li><a href="?page=social">redes sociais</a></li>
+					<li><a href="?page=dominio">registre seu domínio</a></li>
+					<li><a href="?page=fale">contate-nos</a></li>
+				</ul>
+				<div class="c"><!--  --></div>
+			</span>
+		</div>
+		</span>
+		<div class="b l p5"><!--  --></div>
+		<div class="b l p10">
+			<?php include("includes/zoom.php"); ?>
+		</div>
+		<div class="b l p5"><!--  --></div>
+		<div class="b l" id="fb-root"><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like href="http://www.infoclad.com.br" send="true" show_faces="false"></fb:like></div>
+		<div class="b r p5">
+			<span class="b l"><input type="text" class="busca" value="Digite uma palavra chave"/></span>
+			<span class="b l p5"></span>
+			<span class="b l submit">ok</span>
+			<div class="c"><!--  --></div>
+		</div>		
+		<div class="c"><!--  --></div>
+	</div>
+</div>
+<?php include("includes/breadcrumb.php"); ?>
+<div class="b p3"><!--  --></div>
+<div class="b">
+	<div class="b l destaque">
+		<span class="b destaque-pic"><img src="img/destaque1.gif"/></span>
+		<span class="b destaque-link"><a class="green-box" href="?page=">Saiba &#10513;</a></span>
+	</div>
+	<div class="b l destaque mid">
+		<span class="b destaque-pic"><img src="img/destaque2.gif"/></span>
+		<span class="b destaque-link"><a class="blue-box" href="?page=">Saiba &#10513;</a></span>
+	</div>
+	<div class="b l destaque">
+		<span class="b destaque-pic"><img src="img/destaque3.gif"/></span>
+		<span class="b destaque-link"><a class="red-box" href="?page=">Saiba &#10513;</a></span>
+	</div>
+	<div class="c"><!--  --></div>
+</div>
