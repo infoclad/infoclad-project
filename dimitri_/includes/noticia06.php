7dcff161a9a381d9ac6ef15328f049a3fe15fe9a
Eder <eder@infoclad.com.br>
2011-06-29 20:06:16 -0300

    update

---
+++ dimitri_/includes/noticia06.php
@@ -0,0 +1,23 @@
+<div class="b nav-banner"><img src="banner/content/images/navbanner.gif"/></div>
+<div class="b p5"><!--  --></div>
+<div class="b">
+	<span class="b r"><?php include("includes/zoom.php");?></span>
+	<div class="c"><!--  --></div>
+</div>
+<div class="b p5"><!--  --></div>
+<div class="b">
+<span class="b">
+		<span class="b l bluebox">2011</span>
+		<span class="b l p3"><!--  --></span>
+		<span class="b l p5">21 JUN</span>
+		<div class="c p5"><!--  --></div>
+		<span class="b"><h1>Reforçamos que não haverá aula no dia 22/06 (4ª feira) em razão da aula-passeio com as turmas do 5º ano.</h1></span>
+		<span class="b p3"><!--  --></span>
+		<span class="b dotted">
+		<p class="paragrafo-conteudo" id="noticia02">
+<a href="files/passpeda-5.pdf"><strong>Leia o comunicado completo aqui.</strong></a>
+		</p>
+		</span>
+</span>
+
+</div>
