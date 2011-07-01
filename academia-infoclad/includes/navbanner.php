003b9097cce26a80fe6f65e500e2a26a7dbd6a0c
Eder <eder@infoclad.com.br>
2011-07-01 16:42:14 -0300

    update

--- academia-infoclad/includes/navbanner.php
+++
@@ -1,9 +0,0 @@
-<div class="b navbanner">
-	<div class="b wrapper">
-			<div id="imageContainer"> 
-									<img src="img/banner/acad.gif">
-									<img src="img/banner/acad.gif">
-									<img src="img/banner/acad.gif">
-			</div>
-	</div>
-</div>
