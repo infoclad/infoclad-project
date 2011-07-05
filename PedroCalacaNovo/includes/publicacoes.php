f696a187dbc2556d275c1ee8f37567b0c37dcd1d
Eder <eder@infoclad.com.br>
2011-07-05 15:26:11 -0300

    calaca publicacoes

---
+++ PedroCalacaNovo/includes/publicacoes.php
@@ -0,0 +1,65 @@
+				<?php include("includes/tiny_banner.php"); ?>
+				<div class="b p5"><!--  --></div>
+				<?php include("includes/breadcrumb.php"); ?>
+				<div class="b p5">
+					<?php include("includes/sidebar.php"); ?>
+					<div class="b l grayText conteudo">
+						<div class="b">
+							<h2 class="b l">Título da Página</h2>
+							<span class="b l page-descr"><span class="pinkText">&#187;</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span>
+							<div class="c p5"><!--  --></div>
+						</div>
+						<div class="b">
+						<!-- conteudo -->
+								<div class="b l p5" style="padding-left:0;">
+									<div class="p5 blackbox floatcell">
+										<span class="b"><a href="?page=view"><img width="150" src="img/publica.jpg"/></a></span>
+										<span class="b">
+											<p><i>NEWS Magazine<br/>Lorem 2011</i></p>
+											<p><a class="whiteText" href="?page=view">Traduction conferentias nos lo. Regno international per es. Terra magazines duo lo, e uso libro anteriormente, al pro basate esseva association. Ma celo abstracte via, su germano giuseppe excellente sed.</a></p>
+										</span>
+									</div>
+								</div>
+								<div class="b l p5" style="padding-left:0;">
+									<div class="p5 blackbox floatcell">
+										<span class="b"><a href="?page=view"><img width="150" src="img/publica.jpg"/></a></span>
+										<span class="b">
+											<p><i>NEWS Magazine<br/>Lorem 2011</i></p>
+											<p><a class="whiteText" href="?page=view">Traduction conferentias nos lo. Regno international per es. Terra magazines duo lo, e uso libro anteriormente, al pro basate esseva association. Ma celo abstracte via, su germano giuseppe excellente sed.</a></p>
+										</span>
+									</div>
+								</div>
+								<div class="b l p5" style="padding-left:0;">
+									<div class="p5 blackbox floatcell">
+										<span class="b"><a href="?page=view"><img width="150" src="img/publica.jpg"/></a></span>
+										<span class="b">
+											<p><i>NEWS Magazine<br/>Lorem 2011</i></p>
+											<p><a class="whiteText" href="?page=view">Traduction conferentias nos lo. Regno international per es. Terra magazines duo lo, e uso libro anteriormente, al pro basate esseva association. Ma celo abstracte via, su germano giuseppe excellente sed.</a></p>
+										</span>
+									</div>
+								</div>
+								<div class="b l p5" style="padding-left:0;">
+									<div class="p5 blackbox floatcell">
+										<span class="b"><a href="?page=view"><img width="150" src="img/publica.jpg"/></a></span>
+										<span class="b">
+											<p><i>NEWS Magazine<br/>Lorem 2011</i></p>
+											<p><a class="whiteText" href="?page=view">Traduction conferentias nos lo. Regno international per es. Terra magazines duo lo, e uso libro anteriormente, al pro basate esseva association. Ma celo abstracte via, su germano giuseppe excellente sed.</a></p>
+										</span>
+									</div>
+								</div>
+								<div class="b l p5" style="padding-left:0;">
+									<div class="p5 blackbox floatcell">
+										<span class="b"><a href="?page=view"><img width="150" src="img/publica.jpg"/></a></span>
+										<span class="b">
+											<p><i>NEWS Magazine<br/>Lorem 2011</i></p>
+											<p><a class="whiteText" href="?page=view">Traduction conferentias nos lo. Regno international per es. Terra magazines duo lo, e uso libro anteriormente, al pro basate esseva association. Ma celo abstracte via, su germano giuseppe excellente sed.</a></p>
+										</span>
+									</div>
+								</div>
+								<div class="c p5"><!--  --></div>
+								<?php include("includes/paginacao.php"); ?>
+						<!-- conteudo -->
+						</div>
+					</div>
+					<div class="c"><!--  --></div>
+				</div>
