387c4bc69d497de4a0d3ed2da6cfa934ee4d297a
Wesley Andrade <eder@infoclad.com.br>
2011-07-20 16:32:05 -0300

    infoclad 2

---
+++ Infoclad/site2/includes/header.php
@@ -0,0 +1,6 @@
+<div class="b topbar">
+	<div class="wrapper"><!--  --></div>
+</div>
+<div class="b header">
+	<div class="wrapper"><!--  --></div>
+</div>
---
+++ Infoclad/site2/includes/menu.php
@@ -0,0 +1,53 @@
+<!-- horizontal menu module -->
+<div id="mainMenuHolder">
+    <div id="mainMenu" class="b">
+        <div class="wrapper">
+            <ul class="b l inline">
+                <li>
+                    <a href="?page=home"><div class="b l mainLink">Home</div></a>
+                </li>
+                <li>
+                    <a href="?page=institucional"><div class="b l mainLink">A Marca</div></a>
+                </li>
+                <li class="aSub">
+                    <a href="?page=fornecedores"><div class="b l mainLink">Linhas</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=street">linha street</a></li>
+                        <li><a href="?page=style">linha style</a></li>
+                        <li><a href="?page=cult">linha cult</a></li>
+                    </ul>
+                </li>
+                <li class="aSub">
+                    <a href="?page=produtos"><div class="b l mainLink">Produtos</div></a>
+                    <ul class="subMainMenu inline">
+                        <li><a href="?page=produtos">camisas</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="?page=home"><div class="b l mainLink">Concurso</div></a>
+                </li>
+                <li>
+                    <a href="?page=home"><div class="b l mainLink">Comunidade</div></a>
+                </li>
+                <li>
+                    <a href="?page=home"><div class="b l mainLink">Ajuda</div></a>
+                </li>
+                <li>
+                    <a href="?page=home"><div class="b l mainLink">Contato</div></a>
+                </li>
+                <div class="c"><!--  --></div>				
+            </ul>
+            <div id="loginBox" class="b l">
+            	<table>
+            		<tr>
+	            		<td><input class="login" type="text" value="login"/></td>
+	            		<td><input class="login" type="password"/></td>
+	            		<td><a href="#"><button class="sprite submit"></button></a></td>
+            		</tr>
+            	</table>
+            </div>
+            <div class="c"><!--  --></div>
+        </div>
+    </div>
+</div>
+<!-- horizontal menu module -->
