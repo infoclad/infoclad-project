8a2917813c6297304113151b81791251a8864cd8
Wesley Andrade <eder@infoclad.com.br>
2011-07-21 13:50:23 -0300

    infoclad

--- Infoclad/site2/includes/header.php
+++ Infoclad/site2/includes/header.php
@@ -1,6 +1,44 @@
-<div class="b topbar">
-	<div class="wrapper"><!--  --></div>
-</div>
-<div class="b header">
-	<div class="wrapper"><!--  --></div>
+<div class="b wrapper header">
+	<a class="b l logo" href="?page=home"><!-- --></a>
+	<div class="b r">
+		<span class="b">
+			<ul class="inline">
+				<li><a href="?page=fale">Contate-nos</a></li>
+				<li>|</li>
+				<li><a href="?page=fale">Infoclad Host</a></li>
+				<li>|</li>
+				<li><a href="?page=fale">Academia Infoclad</a></li>
+			</ul>			
+		</span>
+		<span class="b">
+			<span class="b l"><select><option>Serviços</option></select></span>
+			<span class="b l p5"><!-- --></span>
+			<span class="b l login">
+				<span class="b l"><input type="text"/></span>
+				<span class="b l"><input type="text"/></span>
+				<a href="#" class="b l sprite ok_login"><!-- --></a>
+				<div class="c"><!-- --></div>
+			</span>
+			<div class="c"><!-- --></div>
+		</span>
+		<span class="b p5"><!-- --></span>
+		<div class="b menu">
+			<a href="?page=home"  class="b l sprite home_icon"><!-- --></a>
+			<span class="b l p5"><!-- --></span>
+			<a class="b l" href="?page=infoclad">A Infoclad</a>
+			<span class="b l p5"><!-- --></span>
+			<a class="b l" href="?page=solucoes">Soluções</a>
+			<span class="b l p5"><!-- --></span>
+			<a class="b l" href="?page=clientes">Clientes</a>
+			<span class="b l p5"><!-- --></span>
+			<a class="b l" href="?page=fale">Contate-nos</a>
+			<div class="b r">
+				<span class="b l"><input type="text"/></span>
+				<a href="#" class="b sprite ok_busca"><!-- --></a>
+				<div class="c"><!-- --></div>
+			</div>
+			<div class="c"><!-- --></div>
+		</div>
+	</div>
+	<div class="c"><!-- --></div>
 </div>
